d491471ad40d8786e5db2711032ff434b561f3eb
harsuuu <harshjoshi7895@gmail.com>
2022-09-05 00:54:49 +0530

    Nested loop-->Question and concept

---
+++ SECTION_4_ARRAY/nexted loops/nested_loop5.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+int count=1;
+for (int i=1;i<=4;i++)
+{
+    for(int j=1;j<=4;j++)
+    {
+        cout<<  count  ;
+        count++;
+    }
+    cout<<endl;
+}
+return 0;
+}
---
+++ SECTION_4_ARRAY/nexted loops/nexted_loop6.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+for(int i=1;i<=4;i++)
+{
+    for(int j=i;j<=4;j++)
+    {
+        cout<<"*";
+    }
+    cout<<endl;
+}
+return 0;
+}
