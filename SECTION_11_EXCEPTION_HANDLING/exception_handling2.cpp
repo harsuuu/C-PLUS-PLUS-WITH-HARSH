0114ce3b6b4c2367fec29e387c25fe16080275a8
harsuuu <harshjoshi7895@gmail.com>
2023-01-01 14:52:57 +0530

    Basics of Exception Handling

---
+++ SECTION_11_EXCEPTION_HANDLING/exception_handling2.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=10,b=5,c;
+
+    try
+    {
+        if(b==0)
+            throw 101;
+        c=a/b;
+        cout<<c<<endl;
+    }
+    catch(int e)
+    {
+        cout<<"error code : "<<e<<endl;
+    }
+
+    cout<<"BYE";
+    
+
+    return 0; 
+}
